37944af3c9c33ae657d79a441038aa2dc8fec319
Alexander Pronin <pronin.alx@gmail.com>
2016-10-18 00:23:44 +0300

    REST API with Express, MongoDB and Mongoose

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,38 +1,60 @@
 var express = require('express');
-var leaderRouter = express.Router();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Leaders = require('../models/leadership');
+
+var leaderRouter = express.Router();
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-.all(function(req,res,next){
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      next();
-})
 .get(function(req,res,next){
-        res.end('Will send all the leaders information to you!');
+    Leaders.find({}, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
 })
 .post(function(req, res, next){
-    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);    
+    Leaders.create(req.body, function (err, leader) {
+        if (err) throw err;
+        console.log('Leader created!');
+        var id = leader._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the leader with id: ' + id);
+    });
 })
 .delete(function(req, res, next){
-        res.end('Deleting all leaders');
+    Leaders.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
 });
 
 leaderRouter.route('/:id')
-.all(function(req,res,next) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      next();
-})
 .get(function(req,res,next){
-        res.end('Will send details of the leader: ' + req.params.id +' to you!');
+    Leaders.findById(req.params.id, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
 })
 .put(function(req, res, next){
-    res.write('Updating the leader: ' + req.params.id + '\n');
-    res.end('Will update the leader: ' + req.body.name + 
-            ' with details: ' + req.body.description);
+    Leaders.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
 })
 .delete(function(req, res, next){
-        res.end('Deleting leader: ' + req.params.id);
+    Leaders.findByIdAndRemove(req.params.id, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
 });
 
 module.exports = leaderRouter;
